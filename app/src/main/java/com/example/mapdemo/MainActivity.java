ff1fd63544c263f24b3f5a8f48b6b22f85b36239
Ferguson <834929497@qq.com>
2021-02-21 23:10:33 +0800

    增加了运行时定位权限申请

--- app/src/main/java/com/example/mapdemo/MainActivity.java
+++ app/src/main/java/com/example/mapdemo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.mapdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,10 +30,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.start:
-                Toast.makeText(MainActivity.this,"进入高德地图",Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, BasicFragmentActivity.class);
-                startActivity(intent);
+                if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.
+                        permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new
+                            String[]{ Manifest.permission.ACCESS_COARSE_LOCATION},1);
+                }else{
+                    getLocationPermission();
+                }
+
                 break;
+        }
+    }
+    private void getLocationPermission(){
+        try{
+            Toast.makeText(MainActivity.this,"进入高德地图",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, BasicFragmentActivity.class);
+            startActivity(intent);
+        }catch (SecurityException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    getLocationPermission();
+                }else{
+                    Toast.makeText(this,"您拒绝了授权",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this,"进入高德地图__无法定位",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, BasicFragmentActivity.class);
+                    startActivity(intent);
+                }
+                break;
+            default:
         }
     }
 }
